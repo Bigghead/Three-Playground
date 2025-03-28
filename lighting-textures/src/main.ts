1e47f1e7e86143696faf58ef79b143edbb0c4fcb
Bigghead <shavenchew@yahoo.com>
2025-03-27 22:13:59 -0700

    new textures and point light

--- lighting-textures/src/main.ts
+++ lighting-textures/src/main.ts
@@ -36,17 +36,42 @@ controls.enableDamping = true;
  * Lighting
  */
 const ambientLight = new three.AmbientLight()
-scene.add(ambientLight)
+const pointLight = new three.PointLight(0xffffff, 50)
+pointLight.position.set(2, 3, 4)
+scene.add(ambientLight, pointLight)
+
+
+/**
+ * Textures
+ */
+const textureLoader = new three.TextureLoader()
+const textureMap = {
+  door: textureLoader.load('/textures/color.jpg'),
+  metal_sheet: textureLoader.load('/textures/metal_sheet.jpg'),
+  blue_metal: textureLoader.load('/textures/blue_metal.jpg'),
+  coast_land: textureLoader.load('/textures/coast_land.jpg'),
+  rock_wall: textureLoader.load('/textures/rock_wall.jpg'),
+}
+textureMap.door.colorSpace = three.SRGBColorSpace
+textureMap.metal_sheet.colorSpace = three.SRGBColorSpace
+// textureMap.metal_sheet.magFilter = three.NearestFilter;
+// textureMap.metal_sheet.minFilter = three.LinearMipMapLinearFilter;
+textureMap.blue_metal.colorSpace = three.SRGBColorSpace
+textureMap.coast_land.colorSpace = three.SRGBColorSpace
+textureMap.rock_wall.colorSpace = three.SRGBColorSpace
+
 
 
 /**
  * Objects
  */
-const geometry = new three.SphereGeometry()
+const geometry = new three.SphereGeometry(1, 64)
 
 // need lighting?
 const material = new three.MeshPhysicalMaterial({
-  color: 'red'
+  // color: 'red'
+  map: textureMap.metal_sheet,
+  sheen: 1
 })
 scene.add(new three.Mesh(geometry, material))
 console.log(scene)
