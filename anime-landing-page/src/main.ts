1119aca34dbc3f4d8a6e85d08b373c472781780f
Bigghead <shavenchew@yahoo.com>
2025-04-17 22:20:47 -0700

    1st pass continuous videos switch through

--- anime-landing-page/src/main.ts
+++ anime-landing-page/src/main.ts
@@ -1,19 +1,26 @@
+import { gsap } from "gsap";
+
 const elements = {
-  "video-container": document.querySelector(
-    ".video-container video"
+  "video-current": document.querySelector(
+    ".video-current video"
   ) as HTMLVideoElement,
   "video-preview": document.querySelector(
     ".video-preview video"
+  ) as HTMLVideoElement,
+  "invisible-video-preview": document.querySelector(
+    ".video-preview video.invisible-preview"
   ) as HTMLVideoElement,
 };
 
 const videoLength = 4;
 let currentVideo = 1;
+let hasFinishedLoadingAnimation = true;
 
 /**
  * Event Listeners
  */
 elements["video-preview"]?.addEventListener("click", () => {
+  if (!hasFinishedLoadingAnimation) return;
   startNextVideo();
 });
 
@@ -21,13 +28,39 @@ elements["video-preview"]?.addEventListener("click", () => {
  * Loop through available video previews and use preview src to switch playing video
  */
 function startNextVideo(): void {
+  hasFinishedLoadingAnimation = false;
   // play video
   const currentVideoIndex = currentVideo % videoLength;
-  const video = elements["video-container"];
+  const video = elements["video-current"];
   const videoPreview = elements["video-preview"];
-  video.src = `hero-${currentVideoIndex + 1}.mp4`;
-  video.load();
-  video.play();
+  const invisibleVideoPreview = elements["invisible-video-preview"];
+  // video.src = `hero-${currentVideoIndex + 1}.mp4`;
+  // video.load();
+  // video.play();
+  invisibleVideoPreview.src = `hero-${currentVideoIndex + 1}.mp4`;
+  invisibleVideoPreview.load();
+  invisibleVideoPreview.play().then(() => {
+    const tl = gsap.timeline();
+    tl.to(invisibleVideoPreview, {
+      visibility: "visible",
+      opacity: 1,
+      zIndex: 20,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      duration: 1.5,
+      ease: "power3.out",
+      onComplete: () => {
+        hasFinishedLoadingAnimation = true;
+      },
+    });
+    tl.set(invisibleVideoPreview, {
+      visibility: "hidden",
+      opacity: 1,
+      zIndex: 5,
+      width: "300px",
+      height: "200px",
+    });
+  });
 
   // change video preview src to the next video
   videoPreview.src = `hero-${
