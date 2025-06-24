e4ff9f09120ec43b72d7e6d6484195851e42cfad
Bigghead <shavenchew@yahoo.com>
2025-06-23 18:41:44 -0700

    1st pass create flat hexagon map

---
+++ geometric-terrain-map/src/lib/utils.ts
@@ -0,0 +1,5 @@
+import * as three from "three";
+
+export const positionNeighbors = (x: number, y: number): three.Vector3 => {
+  return new three.Vector3((x + (y % 2) * 0.5) * 2, 0, y * 1.7);
+};
--- geometric-terrain-map/src/main.ts
+++ geometric-terrain-map/src/main.ts
@@ -1,20 +1,43 @@
 import * as three from "three";
 import { ThreeCanvas } from "./canvas";
+import { positionNeighbors } from "./lib/utils";
 
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 if (!canvas) {
   console.error("Canvas element with class 'webgl' not found.");
 }
 
+const hexagonGroupWidth = 5;
+
 const threeCanvas = new ThreeCanvas({
   canvas,
   initShadow: false,
 });
 
-const cube: three.Mesh<three.BoxGeometry, three.MeshBasicMaterial> =
-  new three.Mesh(
-    new three.BoxGeometry(1, 1, 1),
-    new three.MeshBasicMaterial({ color: 0x00ff00 })
-  );
+const basicMaterial = new three.MeshStandardMaterial({
+  color: 0x00ff00,
+  flatShading: true,
+});
 
-threeCanvas.scene.add(cube);
+threeCanvas.scene.add(new three.AxesHelper(20));
+
+const createHexagons = (): three.Group => {
+  const hexagonGroup = new three.Group();
+
+  for (let i = -hexagonGroupWidth; i < hexagonGroupWidth; i++) {
+    for (let j = -hexagonGroupWidth; j < hexagonGroupWidth; j++) {
+      const hexagon = new three.Mesh(
+        new three.CylinderGeometry(1, 1, 1, 6, 1, false),
+        basicMaterial
+      );
+      const { x, y, z } = positionNeighbors(i, j);
+      hexagon.position.set(x, y, z);
+      hexagonGroup.add(hexagon);
+    }
+  }
+
+  return hexagonGroup;
+};
+
+const hexagonGroup = createHexagons();
+threeCanvas.scene.add(hexagonGroup);
