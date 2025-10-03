fa913ee5945dbbab34c92d28134e5ade5ccde740
bigghead <shavenchew@yahoo.com>
2025-10-03 16:42:04 -0700

    event delegation, baybeeee

--- room-configurator/src/lib/constants.ts
+++ room-configurator/src/lib/constants.ts
@@ -4,63 +4,73 @@ export const INACTIVE_MODEL_EVENT = "inactive model event";
 export const WALL_DIVIDER = "wall divider";
 
 export const DomEl = {
-	get canvas() {
-		return document.querySelector("canvas.webgl") as HTMLCanvasElement;
-	},
+    get canvas() {
+        return document.querySelector("canvas.webgl") as HTMLCanvasElement;
+    },
 
-	get configurator() {
-		return document.querySelector(".configurator") as HTMLDivElement;
-	},
-	get configuratorContent() {
-		return document.querySelector(".configurator-content") as HTMLDivElement;
-	},
-	get configSidebar() {
-		return document.querySelector(".configurator-sidebar") as HTMLDivElement;
-	},
-	get configSidebarMenu() {
-		return document.querySelectorAll(
-			".configurator-sidebar > .menu"
-		) as NodeListOf<HTMLDivElement>;
-	},
+    get configurator() {
+        return document.querySelector(".configurator") as HTMLDivElement;
+    },
+    get configuratorContent() {
+        return document.querySelector(
+            ".configurator-content"
+        ) as HTMLDivElement;
+    },
+    get configSidebar() {
+        return document.querySelector(
+            ".configurator-sidebar"
+        ) as HTMLDivElement;
+    },
+    get configSidebarMenu() {
+        return document.querySelectorAll(
+            ".configurator-sidebar > .menu"
+        ) as NodeListOf<HTMLDivElement>;
+    },
 
-	get configModal() {
-		return document.querySelector(".config-modal") as HTMLDivElement;
-	},
-	get modelRotationSlider() {
-		return document.querySelector(
-			".config-modal input#model-rotation"
-		) as HTMLInputElement;
-	},
-	get modelRotationText() {
-		return document.querySelector(
-			".config-modal .item-rotation"
-		) as HTMLSpanElement;
-	},
+    get configModal() {
+        return document.querySelector(".config-modal") as HTMLDivElement;
+    },
+    get modelRotationSlider() {
+        return document.querySelector(
+            ".config-modal input#model-rotation"
+        ) as HTMLInputElement;
+    },
+    get modelRotationText() {
+        return document.querySelector(
+            ".config-modal .item-rotation"
+        ) as HTMLSpanElement;
+    },
 
-	get modelWidthSliderContainer() {
-		return document.querySelector(
-			".config-modal-slider--container__hidden"
-		) as HTMLDivElement;
-	},
-	get modelWidthSlider() {
-		return document.querySelector(
-			".config-modal input#model-width"
-		) as HTMLInputElement;
-	},
-	get modelWidthText() {
-		return document.querySelector(
-			".config-modal .item-width"
-		) as HTMLSpanElement;
-	},
+    get modelWidthSliderContainer() {
+        return document.querySelector(
+            ".config-modal-slider--container__hidden"
+        ) as HTMLDivElement;
+    },
+    get modelWidthSlider() {
+        return document.querySelector(
+            ".config-modal input#model-width"
+        ) as HTMLInputElement;
+    },
+    get modelWidthText() {
+        return document.querySelector(
+            ".config-modal .item-width"
+        ) as HTMLSpanElement;
+    },
 
-	get resetButton() {
-		return document.querySelector(
-			".config-modal-action #reset-btn"
-		) as HTMLButtonElement;
-	},
-	get deleteButton() {
-		return document.querySelector(
-			".config-modal-action #delete-btn"
-		) as HTMLButtonElement;
-	},
+    get resetButton() {
+        return document.querySelector(
+            ".config-modal-action #reset-btn"
+        ) as HTMLButtonElement;
+    },
+    get deleteButton() {
+        return document.querySelector(
+            ".config-modal-action #delete-btn"
+        ) as HTMLButtonElement;
+    },
+
+    get roomConfigRowElement() {
+        return document.querySelector(
+            ".room-configurator .content"
+        ) as HTMLDivElement;
+    },
 };
--- room-configurator/src/main.ts
+++ room-configurator/src/main.ts
@@ -1,167 +1,188 @@
 import {
-	DomEl,
-	ACTIVE_MODEL_CLICKED,
-	INACTIVE_MODEL_EVENT,
-	WALL_DIVIDER,
+    DomEl,
+    ACTIVE_MODEL_CLICKED,
+    INACTIVE_MODEL_EVENT,
+    WALL_DIVIDER,
 } from "./lib/constants";
 import { models, type ModelVector3 } from "./lib/model-configs";
 import type { ModelType, ModelName } from "./lib/types";
 
 document.addEventListener("DOMContentLoaded", async () => {
-	const {
-		renderModel,
-		createWall,
-		rotateModel,
-		editWidthModel,
-		resetModelChanges,
-		removeActiveModel,
-	} = await import("./canvas");
+    const {
+        renderModel,
+        createWall,
+        rotateModel,
+        editWidthModel,
+        resetModelChanges,
+        removeActiveModel,
+    } = await import("./canvas");
 
-	let activeMenu = "home";
-	const originalWallWidth = 3;
+    let activeMenu = "home";
+    const originalWallWidth = 3;
 
-	/**
-	 * Elements
-	 */
+    /**
+     * Elements
+     */
 
-	const handleModelImageClick =
-		(modelType: ModelType, modelKey: ModelName) => async () => {
-			try {
-				if (modelType === "wall") {
-					return createWall();
-				}
+    const handleModelImageClick =
+        (modelType: ModelType, modelKey: ModelName) => async () => {
+            try {
+                if (modelType === "wall") {
+                    return createWall();
+                }
 
-				await renderModel(modelType, modelKey);
-			} catch (e) {
-				console.error(e);
-			}
-		};
+                await renderModel(modelType, modelKey);
+            } catch (e) {
+                console.error(e);
+            }
+        };
 
-	const clearModelImages = (): void => {
-		DomEl.configuratorContent.innerHTML = "";
-	};
+    const clearModelImages = (): void => {
+        DomEl.configuratorContent.innerHTML = "";
+    };
 
-	const renderModelImages = (modelType: ModelType): void => {
-		clearModelImages();
-		const chosenModels = models[modelType];
+    const renderModelImages = (modelType: ModelType): void => {
+        clearModelImages();
+        const chosenModels = models[modelType];
 
-		for (const key in chosenModels) {
-			const modelKey = key as keyof typeof chosenModels;
-			const { imageUrl } = chosenModels[modelKey];
+        for (const key in chosenModels) {
+            const modelKey = key as keyof typeof chosenModels;
+            const { imageUrl } = chosenModels[modelKey];
 
-			const div = document.createElement("div");
-			div.classList.add("image-container");
+            const div = document.createElement("div");
+            div.classList.add("image-container");
 
-			const img = document.createElement("img");
-			img.src = imageUrl;
-			img.alt = `${modelType} - ${modelKey}`;
+            const img = document.createElement("img");
+            img.src = imageUrl;
+            img.alt = `${modelType} - ${modelKey}`;
 
-			div.appendChild(img);
-			div.addEventListener("click", handleModelImageClick(modelType, modelKey));
+            div.appendChild(img);
+            div.addEventListener(
+                "click",
+                handleModelImageClick(modelType, modelKey)
+            );
 
-			DomEl.configuratorContent.append(div);
-		}
-	};
+            DomEl.configuratorContent.append(div);
+        }
+    };
 
-	/**
-	 * Event Handlers
-	 */
-	const removeCurrentlyActiveMenu = (): void => {
-		const activeMenu = document.querySelector(
-			".configurator-sidebar > .menu.active"
-		);
-		activeMenu?.classList.remove("active");
-	};
+    /**
+     * Event Handlers
+     */
+    const removeCurrentlyActiveMenu = (): void => {
+        const activeMenu = document.querySelector(
+            ".configurator-sidebar > .menu.active"
+        );
+        activeMenu?.classList.remove("active");
+    };
 
-	const hideConfigModal = (): void => {
-		DomEl.configModal.style.visibility = "hidden";
-	};
+    const hideConfigModal = (): void => {
+        DomEl.configModal.style.visibility = "hidden";
+    };
 
-	const initWidthSlider = (scale: ModelVector3): void => {
-		const { x } = scale;
-		DomEl.modelWidthSliderContainer.style.display = "block";
-		const widthValue = (originalWallWidth * x).toString();
-		DomEl.modelWidthText.innerText = widthValue;
-		DomEl.modelWidthSlider.value = widthValue;
-	};
+    const initWidthSlider = (scale: ModelVector3): void => {
+        const { x } = scale;
+        DomEl.modelWidthSliderContainer.style.display = "block";
+        const widthValue = (originalWallWidth * x).toString();
+        DomEl.modelWidthText.innerText = widthValue;
+        DomEl.modelWidthSlider.value = widthValue;
+    };
 
-	const initRotationSlider = (rotation: ModelVector3): void => {
-		const { y } = rotation;
+    const initRotationSlider = (rotation: ModelVector3): void => {
+        const { y } = rotation;
 
-		// need to turn rotation radians back to degrees
-		const degrees = (y * 180) / Math.PI;
-		const normalizedY = Math.round(degrees / 5) * 5;
-		const degreeValue = normalizedY.toString();
+        // need to turn rotation radians back to degrees
+        const degrees = (y * 180) / Math.PI;
+        const normalizedY = Math.round(degrees / 5) * 5;
+        const degreeValue = normalizedY.toString();
 
-		DomEl.modelRotationText.innerText = degreeValue;
-		DomEl.modelRotationSlider.value = degreeValue;
-		DomEl.configModal.style.visibility = "visible";
-	};
+        DomEl.modelRotationText.innerText = degreeValue;
+        DomEl.modelRotationSlider.value = degreeValue;
+        DomEl.configModal.style.visibility = "visible";
+    };
 
-	const resetSliders = (): void => {
-		DomEl.modelWidthText.innerText = originalWallWidth.toString();
-		DomEl.modelWidthSlider.value = originalWallWidth.toString();
-		DomEl.modelRotationText.innerText = "0";
-		DomEl.modelRotationSlider.value = "0";
-		hideConfigModal();
-	};
+    const resetSliders = (): void => {
+        DomEl.modelWidthText.innerText = originalWallWidth.toString();
+        DomEl.modelWidthSlider.value = originalWallWidth.toString();
+        DomEl.modelRotationText.innerText = "0";
+        DomEl.modelRotationSlider.value = "0";
+        hideConfigModal();
+    };
 
-	/**
-	 * Listeners
-	 */
+    /**
+     * Listeners
+     */
 
-	DomEl.configSidebarMenu.forEach((menu): void => {
-		menu.addEventListener("click", (e) => {
-			e.stopPropagation();
+    DomEl.configSidebarMenu.forEach((menu): void => {
+        menu.addEventListener("click", (e) => {
+            e.stopPropagation();
 
-			const target = e.target as HTMLElement;
-			const {
-				dataset: { content },
-			} = target;
+            const target = e.target as HTMLElement;
+            const {
+                dataset: { content },
+            } = target;
 
-			if (content) {
-				removeCurrentlyActiveMenu();
-				activeMenu = content;
-				renderModelImages(content as ModelType);
-				target.classList.add("active");
-			}
-		});
-	});
+            if (content) {
+                removeCurrentlyActiveMenu();
+                activeMenu = content;
+                renderModelImages(content as ModelType);
+                target.classList.add("active");
+            }
+        });
+    });
 
-	DomEl.canvas.addEventListener(ACTIVE_MODEL_CLICKED, (e) => {
-		const custom = e as CustomEvent;
-		DomEl.modelWidthSliderContainer.style.display = "none";
+    DomEl.canvas.addEventListener(ACTIVE_MODEL_CLICKED, (e) => {
+        const custom = e as CustomEvent;
+        DomEl.modelWidthSliderContainer.style.display = "none";
 
-		if (custom.detail.type && custom.detail.type === WALL_DIVIDER) {
-			initWidthSlider(custom.detail.scale);
-		}
+        if (custom.detail.type && custom.detail.type === WALL_DIVIDER) {
+            initWidthSlider(custom.detail.scale);
+        }
 
-		initRotationSlider(custom.detail.rotation);
-	});
+        initRotationSlider(custom.detail.rotation);
+    });
 
-	DomEl.canvas.addEventListener(INACTIVE_MODEL_EVENT, () => {
-		hideConfigModal();
-	});
+    DomEl.canvas.addEventListener(INACTIVE_MODEL_EVENT, () => {
+        hideConfigModal();
+    });
 
-	DomEl.modelRotationSlider.addEventListener("input", (e) => {
-		const target = e.target as HTMLInputElement;
-		DomEl.modelRotationText.innerText = target.value;
-		rotateModel(target.value);
-	});
+    DomEl.modelRotationSlider.addEventListener("input", (e) => {
+        const target = e.target as HTMLInputElement;
+        DomEl.modelRotationText.innerText = target.value;
+        rotateModel(target.value);
+    });
 
-	DomEl.modelWidthSlider.addEventListener("input", (e) => {
-		const target = e.target as HTMLInputElement;
-		DomEl.modelWidthText.innerText = target.value;
-		editWidthModel(target.value);
-	});
+    DomEl.modelWidthSlider.addEventListener("input", (e) => {
+        const target = e.target as HTMLInputElement;
+        DomEl.modelWidthText.innerText = target.value;
+        editWidthModel(target.value);
+    });
 
-	DomEl.resetButton.addEventListener("click", () => {
-		resetModelChanges();
-		resetSliders();
-	});
+    DomEl.resetButton.addEventListener("click", () => {
+        resetModelChanges();
+        resetSliders();
+    });
 
-	DomEl.deleteButton.addEventListener("click", () => {
-		removeActiveModel();
-		resetSliders();
-	});
+    DomEl.deleteButton.addEventListener("click", () => {
+        removeActiveModel();
+        resetSliders();
+    });
+
+    DomEl.roomConfigRowElement.addEventListener("click", (e) => {
+        const target = e.target as HTMLElement;
+        const contentChild = target.closest(".content-child") as HTMLElement;
+
+        if (!target.matches("button") || !contentChild) return;
+
+        console.log(target);
+
+        const btnAction = target.classList.contains("add-btn")
+            ? "Add"
+            : "Subtract";
+
+        console.log(btnAction);
+
+        const dimensionToChange = contentChild.dataset.dimension;
+        console.log(dimensionToChange);
+    });
 });
