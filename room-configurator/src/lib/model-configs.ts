6deecbbcd920bbe888c1389e69fe1a6ba26d9e85
bigghead <shavenchew@yahoo.com>
2025-11-05 17:06:00 -0800

    more manual model configs

--- room-configurator/src/lib/model-configs.ts
+++ room-configurator/src/lib/model-configs.ts
@@ -1,127 +1,161 @@
 export type ModelVector3 = {
-	x: number;
-	y: number;
-	z: number;
+    x: number;
+    y: number;
+    z: number;
 };
 
 export type ModelOffset = {
-	position?: {
-		x?: number;
-		y?: number;
-		z?: number;
-	};
-	rotation?: {
-		x?: number;
-		y?: number;
-		z?: number;
-	};
+    position?: {
+        x?: number;
+        y?: number;
+        z?: number;
+    };
+    rotation?: {
+        x?: number;
+        y?: number;
+        z?: number;
+    };
 };
 
 export type ModelConfig = {
-	url: string;
-	offset?: ModelOffset;
+    url: string;
+    offset?: ModelOffset;
 };
 
 export const modelScaling = {
-	bed: 22.5,
+    bed: 22.5,
 };
 
 const modelRoomScales = {
-	bed: 33,
-	sofa: 45,
-	toilet: 10,
-	sink: 10,
-	shower: 12.5,
+    bed: 33,
+    sofa: 45,
+    table: 25,
+    toilet: 10,
+    sink: 10,
+    shower: 12.5,
 };
 
+/**
+ * This is probably the most annoying part of this project
+ * Every new additional model takes a lot of manual work to be usable before it loads in properly
+ * - convert assets:
+ * 		- convert model to draco ahead of time
+ * 		- compress images to webp
+ * - remove unnecessary planes in the models in blender ( a lot of the creators add invidible floors for some reason )
+ * - set default model position ( the offsets ) cause creators will have wildly different model origins
+ */
+
 export const models = {
-	bed: {
-		bed1: {
-			url: "/models/bed/bed-1-draco.glb",
-			imageUrl: "images/bed/bed-1.webp",
-			roomSizeScale: modelRoomScales.bed,
-			offset: {
-				rotation: {
-					y: Math.PI / 2,
-				},
-			},
-		},
-		bed2: {
-			url: "/models/bed/bed-2-draco.glb",
-			imageUrl: "images/bed/bed-2.webp",
-			roomSizeScale: modelRoomScales.bed,
-			offset: {
-				position: { y: -0.15 },
-				rotation: {
-					y: Math.PI / 2,
-				},
-			},
-		},
+    bed: {
+        bed1: {
+            url: "/models/bed/bed-1-draco.glb",
+            imageUrl: "images/bed/bed-1.webp",
+            roomSizeScale: modelRoomScales.bed,
+            offset: {
+                rotation: {
+                    y: Math.PI / 2,
+                },
+            },
+        },
+        bed2: {
+            url: "/models/bed/bed-2-draco.glb",
+            imageUrl: "images/bed/bed-2.webp",
+            roomSizeScale: modelRoomScales.bed,
+            offset: {
+                position: { y: -0.15 },
+                rotation: {
+                    y: Math.PI / 2,
+                },
+            },
+        },
 
-		bed3: {
-			url: "/models/bed/bed-3-draco.glb",
-			roomSizeScale: modelRoomScales.bed,
-			offset: {
-				position: { y: -0.25 },
-				rotation: {
-					y: -Math.PI / 2,
-				},
-			},
-			imageUrl: "images/bed/bed-3.webp",
-		},
-	},
-	sofa: {
-		sofa1: {
-			url: "/models/sofa/sofa-1-draco.glb",
-			imageUrl: "images/sofa/sofa-1.png",
-			roomSizeScale: modelRoomScales.sofa,
-			offset: {
-				position: { y: 0.7 },
-			},
-		},
-		sofa2: {
-			url: "/models/sofa/sofa-2-draco.glb",
-			imageUrl: "images/sofa/sofa-2.png",
-			roomSizeScale: modelRoomScales.sofa,
-			offset: {
-				position: { y: -0.55 },
-			},
-		},
-		sofa3: {
-			url: "/models/sofa/sofa-3-draco.glb",
-			imageUrl: "images/sofa/sofa-3.png",
-			roomSizeScale: modelRoomScales.sofa,
-		},
-	},
+        bed3: {
+            url: "/models/bed/bed-3-draco.glb",
+            roomSizeScale: modelRoomScales.bed,
+            offset: {
+                position: { y: -0.25 },
+                rotation: {
+                    y: -Math.PI / 2,
+                },
+            },
+            imageUrl: "images/bed/bed-3.webp",
+        },
+    },
+    sofa: {
+        sofa1: {
+            url: "/models/sofa/sofa-1-draco.glb",
+            imageUrl: "images/sofa/sofa-1.webp",
+            roomSizeScale: modelRoomScales.sofa,
+            offset: {
+                // position: { y: 0.7 },
+            },
+        },
+        sofa2: {
+            url: "/models/sofa/sofa-2-draco.glb",
+            imageUrl: "images/sofa/sofa-2.webp",
+            roomSizeScale: modelRoomScales.sofa,
+            offset: {
+                position: { y: -0.55 },
+            },
+        },
+        sofa3: {
+            url: "/models/sofa/sofa-3-draco.glb",
+            imageUrl: "images/sofa/sofa-3.webp",
+            roomSizeScale: modelRoomScales.sofa,
+        },
+    },
 
-	bathroom: {
-		toilet: {
-			url: "/models/bathroom/toilet-draco.glb",
-			imageUrl: "images/sofa/sofa-3.png",
-			roomSizeScale: modelRoomScales.toilet,
-		},
-		shower: {
-			url: "/models/bathroom/shower-draco.glb",
-			imageUrl: "images/sofa/sofa-3.png",
-			roomSizeScale: modelRoomScales.shower,
-			offset: {
-				rotation: {
-					y: Math.PI,
-				},
-			},
-		},
-		sink: {
-			url: "/models/bathroom/sink-draco.glb",
-			imageUrl: "images/sofa/sofa-3.png",
-			roomSizeScale: modelRoomScales.sink,
-			offset: {
-				position: {
-					y: 1.2,
-				},
-			},
-		},
-	},
-	wall: {
-		divider: {},
-	},
+    table: {
+        table1: {
+            url: "/models/table/table-1-draco.glb",
+            imageUrl: "images/table/table-1.webp",
+            roomSizeScale: modelRoomScales.table,
+        },
+        table2: {
+            url: "/models/table/table-2-draco.glb",
+            imageUrl: "images/table/table-2.webp",
+            roomSizeScale: modelRoomScales.table,
+        },
+        table3: {
+            url: "/models/table/table-3-draco.glb",
+            imageUrl: "images/table/table-3.webp",
+            roomSizeScale: modelRoomScales.table,
+        },
+        table4: {
+            url: "/models/table/table-4-draco.glb",
+            imageUrl: "images/table/table-4.webp",
+            roomSizeScale: modelRoomScales.table,
+        },
+    },
+
+    bathroom: {
+        toilet: {
+            url: "/models/bathroom/toilet-draco.glb",
+            imageUrl: "images/sofa/sofa-3.png",
+            roomSizeScale: modelRoomScales.toilet,
+        },
+        shower: {
+            url: "/models/bathroom/shower-draco.glb",
+            imageUrl: "images/sofa/sofa-3.png",
+            roomSizeScale: modelRoomScales.shower,
+            offset: {
+                rotation: {
+                    y: Math.PI,
+                },
+            },
+        },
+        sink: {
+            url: "/models/bathroom/sink-draco.glb",
+            imageUrl: "images/sofa/sofa-3.png",
+            roomSizeScale: modelRoomScales.sink,
+            offset: {
+                position: {
+                    y: 1.2,
+                },
+            },
+        },
+    },
+    wall: {
+        divider: {},
+    },
 };
