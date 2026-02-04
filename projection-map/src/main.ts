2108828a34be6f957aadd15a8efe20b6c381a400
bigghead <shavenchew@yahoo.com>
2026-02-03 21:00:50 -0800

    split texture map to individual grid cell

--- projection-map/src/main.ts
+++ projection-map/src/main.ts
@@ -42,11 +42,37 @@ const createGrid = () => {
 
     for (let x = 0; x <= gridSize; x++) {
         for (let y = 0; y <= gridSize; y++) {
+            const geometry = new three.BoxGeometry(0.65, 0.65, 0.65);
+            const uv = geometry.attributes.uv;
+            console.log(uv);
+
+            // ===== 24 vertices "count" from 6 face cube X 4 edges ===== //
+            for (let i = 0; i < uv.count; i++) {
+                let u = uv.getX(i);
+                let v = uv.getY(i);
+
+                /**
+                 * split each "face" to take in a percentage of the video
+                 * the video grid goes from 0 -> 100% texture or 0 -> 1 in uv sizing
+                 * if the gridsize is 10, each face will take in 10% or 0.1 of the texture
+                 */
+                u /= gridSize;
+                v /= gridSize;
+
+                /**
+                 * shift uv position to take in the next slice of the texture
+                 * if gridsize is 10
+                 * if x is 0, do nothing
+                 * If the cube is at x = 1, we add 0.10 to the coordinates. Now it’s looking the 0.1 - 0.2 mark of the x texture.
+                 */
+                u += x / gridSize;
+                v += y / gridSize;
+
+                uv.setXY(i, u, v);
+            }
             const cube: three.Mesh<three.BoxGeometry, three.MeshBasicMaterial> =
-                new three.Mesh(
-                    new three.BoxGeometry(0.65, 0.65, 0.65),
-                    gridMaterial,
-                );
+                new three.Mesh(geometry, gridMaterial);
+
             cube.position.x = (x - gridSize / 2) * gridSpacing;
             cube.position.y = (y - gridSize / 2) * gridSpacing;
             cube.position.z = 0;
