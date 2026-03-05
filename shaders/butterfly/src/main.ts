6fd64ca772ef0d6da7446575c99ffaace3357be8
bigghead <shavenchew@yahoo.com>
2026-03-04 21:01:18 -0800

    break, clean up

--- shaders/butterfly/src/main.ts
+++ shaders/butterfly/src/main.ts
@@ -2,6 +2,7 @@ import * as three from "three";
 import { ThreeCanvas } from "../../../Shared/three-canvas";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
+import { ButterflyBoid } from "../lib/ButterflyBoid";
 
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 if (!canvas) {
@@ -15,7 +16,7 @@ threeCamera.camera.position.set(0, 0, 20);
 /**
  * Butterfly Setup
  */
-const instancedMeshCount = 1000;
+const instancedMeshCount = 5000;
 
 const butterflyTexture = textureLoader.load("/butterfly-transparent.webp");
 const butterflyGeo = new three.PlaneGeometry(1, 1, 2, 2);
@@ -49,7 +50,6 @@ const butterflyMaterial = new three.ShaderMaterial({
  * Instanced Meshing
  */
 const dummyInstance = new three.Object3D();
-const dummyTempPosition = new three.Vector3(0, 0, 0);
 
 const instancedMesh = new three.InstancedMesh(
     butterflyGeo,
@@ -58,88 +58,9 @@ const instancedMesh = new three.InstancedMesh(
 );
 scene.add(instancedMesh);
 
-class ButterflyBoid {
-    private _maxSpeed = 0.02;
-    private _meshBoundary = 30;
-    private _boundaryMargin = 5; // where to start slowing down from the boundary, or can hard stop at the boundary
-
-    private _position: three.Vector3 = new three.Vector3(
-        (Math.random() - 0.5) * this._meshBoundary,
-        (Math.random() - 0.5) * this._meshBoundary,
-        (Math.random() - 0.5) * this._meshBoundary,
-    );
-    private _velocity: three.Vector3 = new three.Vector3(
-        Math.random() - 0.5,
-        Math.random() - 0.5,
-        Math.random() - 0.5,
-    );
-
-    constructor() {
-        dummyInstance.position.copy(this._position);
-    }
-
-    get position() {
-        return this._position;
-    }
-
-    get velocity() {
-        return this._velocity;
-    }
-
-    public update() {
-        const distance = this._position.length();
-
-        if (distance > this._meshBoundary - this._boundaryMargin) {
-            const pushBack = this.getRandomNewVector3(15)
-                .clone()
-                .sub(this._position)
-                .normalize();
-
-            // change direction
-            // 0.001 means it takes ~20 frames to fully turn around.
-            this._velocity.add(pushBack.multiplyScalar(0.000035));
-        }
-
-        // speed governor, how much butterfly moves each frame
-        this._velocity.clampLength(this._maxSpeed * 0.5, this._maxSpeed);
-
-        // this is the 1st magic, we "add" smooth position vs "set" new position
-        this._position.add(this._velocity);
-    }
-
-    public getMatrix(dummy: three.Object3D) {
-        dummy.position.copy(this._position);
-
-        // need to kinda redo the position update cause we're telling the "mesh" to look forward
-        // we're not moving the mesh here
-        const target = dummyTempPosition
-            .copy(this._position)
-            .add(this._velocity);
-
-        // 2nd magic, tells the head to look at a direction
-        dummy.lookAt(target);
-
-        /**
-         * this "flips" the mesh so its head is pointing towards velocity
-         * we set it last cause "lookAt" above resets the object rotation ( destructive action )
-         */
-        dummy.rotateX(Math.PI / 2);
-        dummy.updateMatrix();
-        return dummy.matrix;
-    }
-
-    private getRandomNewVector3(multiplier: number = 5): three.Vector3 {
-        return new three.Vector3(
-            (Math.random() - 0.5) * multiplier,
-            (Math.random() - 0.5) * multiplier,
-            (Math.random() - 0.5) * multiplier,
-        );
-    }
-}
-
 const butterflies: ButterflyBoid[] = Array.from(
     { length: instancedMeshCount },
-    () => new ButterflyBoid(),
+    () => new ButterflyBoid(dummyInstance),
 );
 
 threeCanvas.addAnimationCallback("butterfly-update", (elapsedTime) => {
